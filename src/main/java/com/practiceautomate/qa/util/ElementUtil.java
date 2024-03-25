f1b3f61f1cac85bd6f081fcdb97f98de24feb3b6
Esther <estherarikilla64@gmail.com>
2024-03-25 20:32:25 +0530

    changed file

--- src/main/java/com/practiceautomate/qa/util/ElementUtil.java
+++ src/main/java/com/practiceautomate/qa/util/ElementUtil.java
@@ -6,6 +6,6 @@ package com.practiceautomate.qa.util;
 public class ElementUtil 
 { 
 	public static void main(String args) {
-	System.out.println("First commit");
+	System.out.println("First commit here");
 	}
 }
